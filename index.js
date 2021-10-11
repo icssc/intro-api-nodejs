7c5214ef2fe2168baa85ecdf51686da07752e466
Raman Gupta <ramanxg@gmail.com>
2021-10-10 18:18:24 -0700

    Uncommented a few lines

--- index.js
+++ index.js
@@ -25,32 +25,32 @@ url = url + '?' + params.toString();
 console.log(url);
 
 
-// // we need to specify options for our request, so define a js object
-// // The options to be used as arguments when we call fetch
-// const options = {
-//     // Each request has a method type, GET, POST, PUT, DELETE
-//     // PeterPortal API most commonly uses GET requests, since we are GETting data.
-//     method: "GET",
-//     // Not necessary for every Web API, but it's good practice
-//     // Ensures our response is in JSON format.
-//     headers : {
-//         "Content-Type": "application/json",
-//     }
+// we need to specify options for our request, so define a js object
+// The options to be used as arguments when we call fetch
+const options = {
+    // Each request has a method type, GET, POST, PUT, DELETE
+    // PeterPortal API most commonly uses GET requests, since we are GETting data.
+    method: "GET",
+    // Not necessary for every Web API, but it's good practice
+    // Ensures our response is in JSON format.
+    headers : {
+        "Content-Type": "application/json",
+    }
 
-//     //There are many other headers and options you can add to your API request, which will largely depend on the API you are using.
-//     // e.g. API keys, CORS, credentials, etc. More can be found in the API or fetch documentation.
-//     // https://developer.mozilla.org/en-US/docs/Web/API/fetch
-// };
+    //There are many other headers and options you can add to your API request, which will largely depend on the API you are using.
+    // e.g. API keys, CORS, credentials, etc. More can be found in the API or fetch documentation.
+    // https://developer.mozilla.org/en-US/docs/Web/API/fetch
+};
 
 
 
 // // Now we send our request! 
-// fetch(url, options)
-//     // Fetch is promised based -- the promise is not fulfilled yet...
-//     .then(response => response.json())
-//     // Now the promise is fulfilled! Let's take a look at the response! 
-//     .then(json => {
-//         console.log(JSON.stringify(json));
-//     })
-//     // Best practice to do error catching! 
-//     .catch(console.error);
+fetch(url, options)
+    // Fetch is promised based -- the promise is not fulfilled yet...
+    .then(response => response.json())
+    // Now the promise is fulfilled! Let's take a look at the response! 
+    .then(json => {
+        console.log(JSON.stringify(json));
+    })
+    // Best practice to do error catching! 
+    .catch(console.error);
